7806a873e6d0b008a691a3ac6da821f0fc566c4d
Andrew <andrikgolova@gmail.com>
2014-05-14 22:33:55 +0300

    Update Config.java

--- Server/src/com/anntie/server/Config.java
+++ Server/src/com/anntie/server/Config.java
@@ -1,7 +1,7 @@
 package com.anntie.server;
 
 public class Config {
-	public static final String HELLO_MESSAGE = "Welocome to Secure Chat Server!";
+	public static final String HELLO_MESSAGE = "Welcome to Secure Chat Server!";
 	public static final int HISTORY_LENGHT = 50;
 	public static final String SERVER_NAME = "SERVER";
 	public static final int PORT = 25576;
